d747a197433589045d6e9cf856775f757b7c80b7
Priyansh Nayak <priyansh.nayak.28@gmail.com>
2026-02-10 00:11:13 +0000

    readding textype cuz everything breaks

--- engine/src/Mesh.cpp
+++ engine/src/Mesh.cpp
@@ -57,7 +57,7 @@ void Mesh::Draw(Shader& shader) {
         GLuint slot;
         std::string uniform;
 
-        //if (!tex->getTextureSlot(tex->type, slot, uniform)) continue;
+        if (!tex->getTextureSlot(tex->type, slot, uniform)) continue;
 
         tex->texUnit(shader, uniform.c_str(), slot);
         tex->Bind(slot);
